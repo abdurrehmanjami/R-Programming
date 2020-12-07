e68d06e696ad4d840cf1ab06e951f30fb261e7f5
abdurrehmanjami <abdurrehmanjami.ar@gmail.com>
2020-12-07 19:35:34 +0500

    done new lines

--- R_code.r
+++ R_code.r
@@ -1,3 +1,11 @@
 #the new file is created for r-programming
 
 print("hello its my first code in r programming")
+
+
+name = "jami"
+version = "the code run fine"
+
+print(name)
+
+print("now commit the all things")
